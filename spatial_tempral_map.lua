ba859b260b6b1eff94263820c9791d67e8b8f047
Zach Dwiel <zdwiel@plotwatt.com>
2015-02-27 02:41:49 +0000

    nice and clean spatial1d

--- spatial_tempral_map.lua
+++ spatial_tempral_map.lua
@@ -49,17 +49,15 @@ function spatial1d(width, inp, outp, kw, dw)
    kh=1
    dh=1
 
-   model = nn.Sequential()
-
-   model:add(nn.Transpose({1,2}))
-   -- batchmode false keeps reshape from heuristically deciding that
-   -- the first dimension is a batch
-   model:add(nn.Reshape(inp, height, width, false))
-
-   conv = nn.SpatialConvolution(inp, outp, kw, kh, dw, dh)
-
-   model:add(conv)
    owidth = (width - kw) / dw + 1
+
+   -- batchmode false on Reshape keeps reshape from heuristically
+   -- deciding that the first dimension is a batch
+
+   model = nn.Sequential()
+   model:add(nn.Transpose({1,2}))
+   model:add(nn.Reshape(inp, height, width, false))
+   model:add(nn.SpatialConvolution(inp, outp, kw, kh, dw, dh))
    model:add(nn.View(outp, owidth))
    model:add(nn.Transpose({1,2}))
 
